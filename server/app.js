a7f7cd91da0734dcbd6a52570f12f25c62b90931
Adrian Prendas Araya <adrian.prendas.araya@est.una.ac.cr>
2019-08-10 14:29:38 -0600

    init

---
+++ server/app.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+// Cargar las rutas
+
+var user_routes = require("./routes/user");
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Configurar las cabeceras http
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONES, PUT, DELETE"
+  );
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+// Carga de rutas base
+app.use("/api", user_routes);
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var mongoose = require("mongoose");
+var app = require("./app");
+var port = process.env.PORT || 8080;
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect("mongodb://localhost:27017/niffler", { useMongoClient: true })
+  .then(() => {
+    console.log("La conexion a la base de datos se realizo correctamente...");
+    app.listen(port, () => {
+      console.log("El servidor local estan corriendo en localhost " + port);
+    });
+  })
+  .catch(err => console.log(err));
