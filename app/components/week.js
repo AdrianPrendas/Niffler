cd1c05d74c948e0a19ca6cfd497635e0c99ec594
Adrian Prendas Araya <adrian.prendas.araya@est.una.ac.cr>
2019-09-29 23:54:10 -0600

    horizontal bar working

--- app/components/week.js
+++ app/components/week.js
@@ -1,113 +1,141 @@
 import React, {Component} from 'react';
 
-import {View, StyleSheet, Modal, Alert, TextInput, Button} from 'react-native';
+import {View, StyleSheet, Modal, Alert, TextInput, Button,AsyncStorage} from 'react-native';
 
-import { BarChart, Grid, XAxis } from 'react-native-svg-charts'
-import * as scale from 'd3-scale'
-import { Text } from 'react-native-svg'
-
+import {BarChart, Grid, XAxis} from 'react-native-svg-charts';
+import * as scale from 'd3-scale';
+import {Text} from 'react-native-svg';
 
 class Week extends Component {
   state = {
-  
-  
-
+    host: 'niffler-rest-api.herokuapp.com',
+    registers: [],
   };
 
- 
+  componentDidMount() {
+    this.loadTable();
+  }
+  retrieveToken = async () => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        return token;
+      }
+    } catch (error) {
+      Alert.alert('Error', `${error}`, [{text: 'Okay'}]);
+    }
+  };
+
+  loadTable() {
+    this.retrieveToken()
+      .then(token => {
+        fetch(`http://${this.state.host}/api/find-all-transactions`, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: token,
+          },
+          method: 'GET',
+        })
+          .then(res => res.json())
+          .then(json => {
+            let {transactions} = json;
+            transactions = transactions.map(t => {
+              t.createdAt = new Date(t.createdAt);
+              return t;
+            });
+            this.setState({registers: transactions});
+          })
+          .catch(err => {
+            Alert.alert('Error', `${err}`, [{text: 'Okay'}]);
+          });
+      })
+      .catch(err => Alert.alert('Error', `${err}`, [{text: 'Okay'}]));
+  }
+
   render() {
- 
-    const data = [
-      {
-          value: 10,
-          label: 'S',
-      },
-      {
-          value: 1,
-          label: 'M',
-      },
-      {
-          value: 30,
-          label: 'T',
-      },
-      {
-          value: 40,
-          label: 'W',
-      },
-      {
-          value: 50,
-          label: 'T',
-      },
-      {
-        value: 60,
-        label: 'F',
-    },
-    {
-      value: 100,
-      label: 'S',
-  },
-  ]
- const CUT_OFF = 20
-  const Labels = ({ x, y, bandwidth, data }) => (
-    data.map((value, index) => (
+    let {registers} = this.state;
+
+    let minDate = 0;
+    let maxDate = 0;
+    dayToDay = [];
+    let data = [];
+
+    if (registers.length != 0) {
+      register = registers.sort(
+        (a, b) => a.createdAt.getDate() - b.createdAt.getDate(),
+      );
+      minDate = registers[0].createdAt.getDate();
+      maxDate = registers[registers.length - 1].createdAt.getDate();
+
+      for (let i = minDate; i <= maxDate; i++) {
+        let day = registers.filter(r => r.createdAt.getDate() === i);
+        dayToDay.push(day);
+      }
+
+      //reverse = dayToDay.reverse();
+
+      data = dayToDay.map(day => {
+        let d = day[0].createdAt.toString().split(' ')[0]
+        let n = day[0].createdAt.toString().split(' ')[2]
+        return {
+          label:  `${d} ${n}` ,
+          value: day.map(r => r.amount).reduce((a, b) => a + b),
+        };
+      });
+    }
+
+    let values = [...data];
+    values.sort((a, b) => a.value - b.value);
+    let CUT_OFF = data.length != 0 ? data[2].value : 0;
+
+    const Values = ({x, y, bandwidth, data}) =>
+      data.map((value, index) => (
         <Text
-            key={ index }
-            x={ x(index) + (bandwidth / 2) }
-            y={ value.value < CUT_OFF ? y(value.value) - 10 : y(value.value) + 15 }
-            fontSize={ 14 }
-            fill={ value.value >= CUT_OFF ? 'white' : 'black' }
-            alignmentBaseline={ 'middle' }
-            textAnchor={ 'middle' }
-        >
-            {value.value}
+          key={index}
+          x={x(index) + bandwidth / 2}
+          y={value.value < CUT_OFF ? y(value.value) - 10 : y(value.value) + 15}
+          fontSize={14}
+          fill={value.value >= CUT_OFF ? 'white' : 'black'}
+          alignmentBaseline={'middle'}
+          textAnchor={'middle'}>
+          {value.value}
         </Text>
-    ))
-)
+      ));
 
+    const Dates = ({x, y, bandwidth, data}) =>
+      data.map((value, index) => (
+        <Text
+          key={value.label}
+          x={x(index) + bandwidth / 2}
+          y={190}
+          fontSize={14}
+          fill={'black'}
+          alignmentBaseline={'middle'}
+          textAnchor={'middle'}>
+          {value.label}
+        </Text>
+      ));
 
-    return (
-      <View style={{flex:1}}>
-        <View style={{flex:1}}>
-
-          
-
-        </View>
-        <View style={{flex:1}}>
-        <BarChart
-            style={{ height: 200}}
-            data={ data }
-            svg={{ fill:'#009688' }}
-            contentInset={{ bottom: 40, }}
-            
-            yAccessor={ ({ item }) => item.value }
-        >
-            <Grid/>
-            <Labels/>
-             <XAxis          
-                    svg={{ fontSize: 10, fontWeight: 'bold' , fill: '#00796B' }}          
-                    style={{ marginTop: 170}}
-                    data={ data }
-                    scale={scale.ScaleBand}
-                    formatLabel={ (value, index) => data[index].label }
-                    labelStyle={ { color: 'black' } }
-                    contentInset={{ left: 20, right: 35 }}
-                />
-        </BarChart>
-        </View>
-
-        
-      </View>
-        
-    )
-}
+    return data.length!=0?(<BarChart
+        style={{height: 200}}
+        data={data}
+        svg={{fill: '#009688'}}
+        contentInset={{top: 30, bottom: 30}}
+        yAccessor={({item}) => item.value}>
+        <Grid />
+        <Values />
+        <Dates />
+      </BarChart>
+    ):(<View></View>)
+  }
 }
 
 const MyStyleSheet = StyleSheet.create({
-    header: {      
-        flex:2,
-        backgroundColor: "#009688",
-        justifyContent: "space-around",
-    }
-    });
+  header: {
+    flex: 2,
+    backgroundColor: '#009688',
+    justifyContent: 'space-around',
+  },
+});
 
 export default Week;
